f546d038fe9e4e8cfa8408616543ee023d474795
Renan Venancio <renan_1419@hotmail.com>
2020-04-17 00:32:18 -0300

    Adicionando validação para evitar emails repetidos

--- src/main/java/com/techzone/digishop/repository/ClientRepository.java
+++ src/main/java/com/techzone/digishop/repository/ClientRepository.java
@@ -1,9 +1,12 @@
 package com.techzone.digishop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techzone.digishop.domain.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Integer>{
-
+	
+	@Transactional(readOnly = true)
+	Client findByEmail(String email);
 }
--- src/main/java/com/techzone/digishop/service/validation/ClientInsertValidator.java
+++ src/main/java/com/techzone/digishop/service/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.techzone.digishop.domain.Client;
 import com.techzone.digishop.domain.ClientType;
 import com.techzone.digishop.dto.ClientNewDTO;
+import com.techzone.digishop.repository.ClientRepository;
 import com.techzone.digishop.resource.exception.FieldMessage;
 import com.techzone.digishop.service.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	
+	@Autowired
+	ClientRepository clientRepository;
 
 	@Override
 	public void initialize(ClientInsert ann) {
@@ -29,6 +36,12 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		if (objectDTO.getType().equals(ClientType.PESSOA_JURIDICA)
 				&& !BR.isValidCNPJ(objectDTO.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+		}
+		
+		Client aux = clientRepository.findByEmail(objectDTO.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
 		}
 
 		for (FieldMessage e : list) {
