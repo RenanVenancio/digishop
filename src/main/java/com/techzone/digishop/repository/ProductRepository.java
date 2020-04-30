e29a008a49cda6c8ffc0aef3922947969cc02e05
Renan Venancio <renan_1419@hotmail.com>
2020-04-30 01:04:44 -0300

    Add serch query

--- src/main/java/com/techzone/digishop/repository/ProductRepository.java
+++ src/main/java/com/techzone/digishop/repository/ProductRepository.java
@@ -1,9 +1,21 @@
 package com.techzone.digishop.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import com.techzone.digishop.domain.Product;
+import com.techzone.digishop.domain.ProductCategory;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+    // @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.category cat WHERE obj.name LIKE %:name% AND cat IN :categories")
+    @Transactional(readOnly = true)
+    Page<Product> findDistinctByNameContainingIgnoreCaseAndCategoryIn(String name, List<ProductCategory> categories, Pageable pageRequest);
+
+    
 
 }
