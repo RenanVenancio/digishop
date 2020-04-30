f29848cc64cf35fb4c3c04a3c549e0f7d903dc81
Renan Venancio <renan_1419@hotmail.com>
2020-04-30 01:03:07 -0300

    nothing

--- src/main/java/com/techzone/digishop/domain/SaleItemPK.java
+++ src/main/java/com/techzone/digishop/domain/SaleItemPK.java
@@ -6,14 +6,16 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+
 @Embeddable
 public class SaleItemPK implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "sale_id")
 	private Sale sale;
+
 	@ManyToOne
 	@JoinColumn(name = "product_id")
 	private Product product;
