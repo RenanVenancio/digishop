f3d5cd7ee6725b446dd8d5fe21a3dfc7d4e1e58e
Renan Venancio <renan_1419@hotmail.com>
2020-05-02 00:52:36 -0300

    Repository com nome antigo

--- src/main/java/com/techzone/digishop/repository/SalePaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techzone.digishop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techzone.digishop.domain.Payment;
-
-public interface SalePaymentRepository extends JpaRepository<Payment, Integer>{
-
-}
