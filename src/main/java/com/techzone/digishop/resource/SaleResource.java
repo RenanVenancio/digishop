5a5b6ff6128c0cd07ba0c344bda134f5e7021fd3
Renan Venancio <renan_1419@hotmail.com>
2020-05-02 00:53:04 -0300

    Criando recurso POST para sale

--- src/main/java/com/techzone/digishop/resource/SaleResource.java
+++ src/main/java/com/techzone/digishop/resource/SaleResource.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+import javax.validation.Valid;
 
 import com.techzone.digishop.domain.Sale;
 import com.techzone.digishop.service.SaleService;
@@ -22,6 +29,15 @@ public class SaleResource {
 	public ResponseEntity<Sale> findById(@PathVariable Integer id){
 		Sale sale = saleService.findById(id);
 		return ResponseEntity.ok().body(sale);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> save(@RequestBody Sale sale) {		
+		System.out.println(sale);
+		saleService.save(sale);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(sale.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
