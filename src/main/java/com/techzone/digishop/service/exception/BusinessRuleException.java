4a79ea6a625c16e124a82f518d30c4393a137232
Renan Venancio <renan_1419@hotmail.com>
2020-05-02 00:54:38 -0300

    Exception para erros de regra de negócio

---
+++ src/main/java/com/techzone/digishop/service/exception/BusinessRuleException.java
@@ -0,0 +1,14 @@
+package com.techzone.digishop.service.exception;
+
+public class BusinessRuleException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+	public BusinessRuleException(String msg) {
+		super(msg);
+	}
+	
+	public BusinessRuleException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
