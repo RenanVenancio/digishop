8b624673fec19d80276727c956c8bae0c866f90c
Renan Venancio <renan_1419@hotmail.com>
2020-05-05 13:49:54 -0300

    Criando JPQL

--- src/main/java/com/techzone/digishop/repository/PaymentRepository.java
+++ src/main/java/com/techzone/digishop/repository/PaymentRepository.java
@@ -1,13 +1,17 @@
 package com.techzone.digishop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 import com.techzone.digishop.domain.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer>{
+    // @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.category cat WHERE obj.name LIKE %:name% AND cat IN :categories")
 
-    //List<Payment> findBy
+    @Query("SELECT obj FROM Payment obj INNER JOIN obj.sale.client client WHERE obj.status = :status AND client.id = :client")
+    List<Payment> findBySaleClientAndStatus(@Param("client") Integer clientId, @Param("status") Integer status);
 
 }
