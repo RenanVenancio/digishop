47408585fb5afcdcc25e02ba410ab39ba2cf9818
Renan Venancio <renan_1419@hotmail.com>
2020-05-06 01:41:45 -0300

    Adicionando Status na JPQL

--- src/main/java/com/techzone/digishop/repository/PaymentRepository.java
+++ src/main/java/com/techzone/digishop/repository/PaymentRepository.java
@@ -11,7 +11,7 @@ import com.techzone.digishop.domain.Payment;
 public interface PaymentRepository extends JpaRepository<Payment, Integer>{
     // @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.category cat WHERE obj.name LIKE %:name% AND cat IN :categories")
 
-    @Query("SELECT obj FROM Payment obj INNER JOIN obj.sale.client client WHERE obj.status = :status AND client.id = :client")
-    List<Payment> findBySaleClientAndStatus(@Param("client") Integer clientId, @Param("status") Integer status);
+    @Query("SELECT obj FROM Payment obj INNER JOIN obj.sale.client client WHERE obj.status = :status AND client.id = :client AND obj.paymentType = :paymentType")
+    List<Payment> findBySaleClientAndStatus(@Param("client") Integer clientId, @Param("status") Integer status, @Param("paymentType") Integer paymentType);
 
 }
