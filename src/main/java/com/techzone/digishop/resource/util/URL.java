089868ad32bbd64c72430f8c225082d8c32f34e1
Renan Venancio <renan_1419@hotmail.com>
2020-05-12 00:20:36 -0300

    Retornando lista vazia

--- src/main/java/com/techzone/digishop/resource/util/URL.java
+++ src/main/java/com/techzone/digishop/resource/util/URL.java
@@ -2,13 +2,16 @@ package com.techzone.digishop.resource.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class URL {
     public static List<Integer> decodeIntList(String s) {
-
+        if(s.equals("")){
+            return new ArrayList<>();
+        }
         return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
 
     }
