8b8c32d586b7633963882169b0b2d458a2285cc1
Renan Venancio <renan_1419@hotmail.com>
2020-05-15 00:36:18 -0300

    Contagem de entradas e saidas

--- src/main/java/com/techzone/digishop/dto/ProductDTO.java
+++ src/main/java/com/techzone/digishop/dto/ProductDTO.java
@@ -3,6 +3,7 @@ package com.techzone.digishop.dto;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.techzone.digishop.domain.Product;
 
 public class ProductDTO implements Serializable {
@@ -18,12 +19,20 @@ public class ProductDTO implements Serializable {
 	private String un = "UN";
 	private BigDecimal weight;
 	private Boolean sell = true;
-	private BigDecimal stock;
 	private String location;
 	private Integer category;
 	private Integer company;
+	private BigDecimal stockEntries;
+	private BigDecimal stockOutputs;
 
-	public ProductDTO(Product product){
+	public ProductDTO(){
+		this.stockEntries = new BigDecimal("0.00");
+		this.stockOutputs = new BigDecimal("0.00");
+		this.purchasePrice = new BigDecimal("0.00");
+		this.salePrice = new BigDecimal("0.00");
+	}
+
+	public ProductDTO(Product product) {
 		this.name = product.getName();
 		this.barcode = product.getBarcode();
 		this.reference = product.getReference();
@@ -33,15 +42,16 @@ public class ProductDTO implements Serializable {
 		this.un = product.getUn();
 		this.weight = product.getWeight();
 		this.sell = product.getSell();
-		this.stock = product.getStock();
 		this.location = product.getLocation();
 		this.category = product.getCategory().getId();
 		this.company = product.getCompany().getId();
+		this.stockEntries = product.getStockEntries();
+		this.stockOutputs = product.getStockOutputs();
 	}
 
 	public ProductDTO(String name, String barcode, String reference, String description, BigDecimal purchasePrice,
-			BigDecimal salePrice, String un, BigDecimal weight, Boolean sell, BigDecimal stock, String location,
-			Integer category, Integer company) {
+			BigDecimal salePrice, String un, BigDecimal weight, Boolean sell, String location, Integer category,
+			Integer company) {
 		this.name = name;
 		this.barcode = barcode;
 		this.reference = reference;
@@ -51,7 +61,6 @@ public class ProductDTO implements Serializable {
 		this.un = un;
 		this.weight = weight;
 		this.sell = sell;
-		this.stock = stock;
 		this.location = location;
 		this.category = category;
 		this.company = company;
@@ -134,11 +143,7 @@ public class ProductDTO implements Serializable {
 	}
 
 	public BigDecimal getStock() {
-		return this.stock;
-	}
-
-	public void setStock(BigDecimal stock) {
-		this.stock = stock;
+		return stockEntries.subtract(stockOutputs);
 	}
 
 	public String getLocation() {
@@ -163,6 +168,24 @@ public class ProductDTO implements Serializable {
 
 	public void setCompany(Integer company) {
 		this.company = company;
+	}
+
+	@JsonIgnore
+	public BigDecimal getStockEntries() {
+		return this.stockEntries;
+	}
+
+	public void setStockEntries(BigDecimal stockEntries) {
+		this.stockEntries = stockEntries;
+	}
+
+	@JsonIgnore
+	public BigDecimal getStockOutputs() {
+		return this.stockOutputs;
+	}
+
+	public void setStockOutputs(BigDecimal stockOutputs) {
+		this.stockOutputs = stockOutputs;
 	}
 
 }
