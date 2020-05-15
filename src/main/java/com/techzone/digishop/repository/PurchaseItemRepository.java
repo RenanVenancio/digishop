bf506bd3034d25972e9418a0040c292ffeb2bc22
Renan Venâncio <32649048+RenanVenancio@users.noreply.github.com>
2020-05-15 11:38:50 -0300

    Consulta de contagem dos Itens comprados

--- src/main/java/com/techzone/digishop/repository/PurchaseItemRepository.java
+++ src/main/java/com/techzone/digishop/repository/PurchaseItemRepository.java
@@ -1,9 +1,20 @@
 package com.techzone.digishop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 import com.techzone.digishop.domain.PurchaseItem;
+import com.techzone.digishop.dto.ProductCountDTO;
 
 public interface PurchaseItemRepository extends JpaRepository<PurchaseItem, Integer>{
 
+    @Query("SELECT " +
+    "    new com.techzone.digishop.dto.ProductCountDTO(i.id.product.id, SUM(i.quantity)) " +
+    "FROM " +
+    "    PurchaseItem i " +
+    "GROUP BY " +
+    "    i.id.product")
+    List<ProductCountDTO> sumItems();
 }
