563d6606d71d803713a2cc8426af3a296134967e
Renan Venancio <renan_1419@hotmail.com>
2020-05-15 23:34:58 -0300

    Completendo consultas de contagem de estoque

--- src/main/java/com/techzone/digishop/repository/ProductRepository.java
+++ src/main/java/com/techzone/digishop/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.techzone.digishop.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,7 +11,6 @@ import java.util.List;
 
 import com.techzone.digishop.domain.Product;
 import com.techzone.digishop.domain.ProductCategory;
-import com.techzone.digishop.dto.ProductCountDTO;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
@@ -18,14 +18,23 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
     @Transactional(readOnly = true)
     Page<Product> findDistinctByNameContainingIgnoreCaseAndCategoryIn(String name, List<ProductCategory> categories, Pageable pageRequest);
     Page<Product> findDistinctByNameContainingIgnoreCase(String name, Pageable pageRequest);
-
-    @Query("SELECT " +
-    "    new com.techzone.digishop.dto.ProductCountDTO(p.id, SUM(i.quantity)) " +
-    "FROM " +
-    "    Product p LEFT JOIN PurchaseItem i ON p.id = i.id.product.id " +
-    "GROUP BY " +
-    "    p.id")
-    List<ProductCountDTO> sumItems();
     
+    
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(
+    "UPDATE Product p " +
+    "SET p.stockEntries = (SELECT COALESCE(SUM(pi.quantity),0) AS qtt FROM PurchaseItem pi WHERE pi.id.product.id = p.id AND pi.id.purchase.cancelled = false) " +
+    "WHERE p.id IN (SELECT DISTINCT pi.id.product.id AS qtt FROM PurchaseItem pi)")
+    void sumPurchases();
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(
+    "UPDATE Product p " +
+    "SET p.stockOutputs = (SELECT COALESCE(SUM(si.quantity),0) AS qtt FROM SaleItem si WHERE si.id.product.id = p.id AND si.id.sale.status != 2) " +
+    "WHERE p.id IN (SELECT DISTINCT si.id.product.id AS qtt FROM SaleItem si)")
+    void sumSales();
+
 
 }
