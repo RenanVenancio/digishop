f0e73ea1a2744365e77d2980f4ad9e0685846e29
Renan Venâncio <32649048+RenanVenancio@users.noreply.github.com>
2020-05-21 11:45:20 -0300

    Criando busca pelo id do cliente

--- src/main/java/com/techzone/digishop/repository/RevenueRepository.java
+++ src/main/java/com/techzone/digishop/repository/RevenueRepository.java
@@ -1,9 +1,11 @@
 package com.techzone.digishop.repository;
 
+import java.util.List;
+
 import com.techzone.digishop.domain.Revenue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RevenueRepository extends JpaRepository<Revenue, Integer>{
-
+    List<Revenue> findByClientId(Integer clientId);
 }
