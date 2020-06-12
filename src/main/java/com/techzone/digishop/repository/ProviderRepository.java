75b6e01bbdf6b68c2de3ce00206bfc639b3879ca
Renan Venancio <renan_1419@hotmail.com>
2020-06-12 14:00:51 -0300

    Adicionando busca

--- src/main/java/com/techzone/digishop/repository/ProviderRepository.java
+++ src/main/java/com/techzone/digishop/repository/ProviderRepository.java
@@ -1,9 +1,12 @@
 package com.techzone.digishop.repository;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.techzone.digishop.domain.Provider;
 
 public interface ProviderRepository extends JpaRepository<Provider, Integer>{
-
+    Page<Provider> findByNameContainingIgnoreCaseOrCpfCnpjContainingIgnoreCase(String name, String cpfCnpj, Pageable pageRequest);
 }
