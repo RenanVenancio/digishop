97ccb757a093851283f82f3049befe717101d34d
Renan Venancio <renan_1419@hotmail.com>
2020-07-05 16:22:13 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/techzone/digishop/config/SecurityConfig.java
+++ src/main/java/com/techzone/digishop/config/SecurityConfig.java
@@ -1,5 +1,46 @@
 package com.techzone.digishop.config;
 
-public class SecurityConfig {
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC_MATCHES = {
+        "/h2-console/**",
+    };
+
+    private static final String[] PUBLIC_MATCHES_GET = {
+        "/products/**",
+        "/categories/**",
+    };
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests()
+        .antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll()
+        .antMatchers(PUBLIC_MATCHES).permitAll()
+        .anyRequest().authenticated();
+    }
+
 }
